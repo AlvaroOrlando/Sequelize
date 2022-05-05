891ddfcbcc285c96c5c25922fc76a93097494bc9
alvaro <alvarocondeorlando@gmail.com>
2022-05-05 13:25:00 -0300

    refatoração

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -2,31 +2,17 @@ const Filme = require('../models/Filmes')
 
 module.exports = {
    
-    teste: (req,res)=>{
-        res.send(req.body)
-    },
     view: (req,res)=>{
         res.render('index')
     },
     async list(req,res) {
         const { genero } = req.body;
-        if(genero == "Comédia"){
             let filmes = await Filme.findAll({
                 where:{
-                    genero: 'Comédia'
+                    genero: genero
                 }
             })
-            res.render('filmes',{filmes});
-        }else if(genero == "Ação"){
-                filmes = await Filme.findAll({
-                    where:{
-                        genero: 'Ação'
-                    }
-                })
-                res.render('filmes',{filmes});
-        } else{
-            res.send('Filme não encontrado!')
-        }
+              res.render('filmes',{filmes});
+         }
     }
-}
 
