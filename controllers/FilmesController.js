2bca24a97f59ef249dbea0f5c36a30d1d585d993
alvaro <alvarocondeorlando@gmail.com>
2022-05-05 01:38:26 -0300

    corrigindo controller

--- controllers/FilmesController.js
+++ controllers/FilmesController.js
@@ -2,7 +2,7 @@ const Filme = require('../models/Filmes')
 
 module.exports = {
     async store(req, res) {
-        const { name, genero } = req.query;
+        const { name, genero } = req.body;
         const filme = await Filme.create({name, genero})
         return res.json(filme)
     },
